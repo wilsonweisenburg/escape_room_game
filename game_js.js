64a502d6198e2b25438e8badeb8c705e2427b9a8
Jon Swain <jonswain@JonsMacBookPro-2.local>
2020-06-03 17:33:02 -0700

    beautify + changed jquery link

--- game_js.js
+++ game_js.js
@@ -1,43 +1,45 @@
 $(document).ready(() => {
-let $target = $("#box1");
-  let $source = $("#box2");
+    let $target = $("#box1");
+    let $source = $("#box2");
 
-  $(document).mousemove((event) => {
-    // First move the source with mouse
-    $source.css({ top: event.clientY, left: event.clientX });
+    $(document).mousemove((event) => {
+        // First move the source with mouse
+        $source.css({
+            top: event.clientY,
+            left: event.clientX
+        });
 
-    // Now see if the source and target have collided
-    // The collision algorithm is in testCollision
-    // If you have more than one target, just loop this..
-    if (testCollision($source, $target) == true) {
+        // Now see if the source and target have collided
+        // The collision algorithm is in testCollision
+        // If you have more than one target, just loop this..
+        if (testCollision($source, $target) == true) {
 
-      $target.removeClass("no-collision-state");
-      $target.addClass("collision-state");
+            $target.removeClass("no-collision-state");
+            $target.addClass("collision-state");
 
-    } else {
+        } else {
 
-      $target.removeClass("collision-state");
-      $target.addClass("no-collision-state");
-    }
-  });
+            $target.removeClass("collision-state");
+            $target.addClass("no-collision-state");
+        }
+    });
 });
 
 
 function testCollision(s, t) {
 
-  if (
-    s.position().left < t.position().left + t.width() &&
-    s.position().left + s.width() > t.position().left &&
-    s.position().top < t.position().top + t.height() &&
-    s.position().top + s.height() > t.position().top
-  ) {
+    if (
+        s.position().left < t.position().left + t.width() &&
+        s.position().left + s.width() > t.position().left &&
+        s.position().top < t.position().top + t.height() &&
+        s.position().top + s.height() > t.position().top
+    ) {
 
-    console.log("collision!");
-    return true;
-  }
+        console.log("collision!");
+        return true;
+    }
 
 
-  console.log("no collision");
-  return false;
+    console.log("no collision");
+    return false;
 }
-
